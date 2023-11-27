76864770f01caadf4845f9251dfd2ba7850653e8
noah <jeongas9810@gmail.com>
2023-11-27 14:34:06 +0900

    feat: Add Controller

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.controller.LottoPlay;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoPlay lottoPlay = new LottoPlay();
+        lottoPlay.start();
+        Console.close();
     }
 }
---
+++ src/main/java/lotto/controller/LottoPlay.java
@@ -0,0 +1,7 @@
+package lotto.controller;
+
+public class LottoPlay {
+    public void start() {
+
+    }
+}
